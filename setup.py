0708d7bb8e0eb4ec1acda53cd0585409e1135145
unaschneck <24469269+unaschneck@users.noreply.github.com>
2022-12-31 21:57:41 -0500

    added descriptors

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_namespace_packages
 
 VERSION="0.0.1"
-DESCRIPTION="A Python package for Access and manipulation of Cassini RADAR images"
+DESCRIPTION="A Python package for access and manipulation of Cassini RADAR images"
 
 with open("README.md", "r") as f:
 	long_description_readme = f.read()
@@ -17,10 +17,23 @@ setup(
 	long_description_content_type='text/markdown',
 	url="https://github.com/unaschneck/pydar",
 	download_url="https://github.com/unaschneck/pydar/archive/refs/tags/v{0}.tar.gz".format(VERSION),
-	author="",
+	author=["Una Schneck (unaschneck)","C. Y. Schneck (cyschneck)"],
 	keywords=[],
-	license="",
-	classifiers=[],
+	license="MIT",
+	classifiers=[
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Education",
+		"Intended Audience :: Science/Research",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.7",
+		"Intended Audience :: Education",
+		"Intended Audience :: Science/Research",
+		"Topic :: Scientific/Engineering :: Physics",
+		"Topic :: Scientific/Engineering :: Visualization",
+		"Topic :: Scientific/Engineering :: Astronomy"
+	],
 	packages=find_namespace_packages(include=['pydar', 'pydar.*']),
 	include_package_data=True,
 	install_requires=[],
