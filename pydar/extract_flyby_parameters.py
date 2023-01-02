678ff0b41c3bc404ce70c8ea9b304c967edf8e23
cyschneck <22159116+cyschneck@users.noreply.github.com>
2023-01-01 21:05:58 -0700

    find CORADR options from jpl website and store

--- pydar/extract_flyby_parameters.py
+++ pydar/extract_flyby_parameters.py
@@ -12,7 +12,7 @@ def getFlybyData():
 	# Header: Titan flyby id, Radar Data Take Number, Sequence number, Orbit Number/ID
 	flyby_id = []
 	flby_radar_take_num = []
-	flyby_csv_file = os.path.join(os.path.dirname(__file__), 'data', 'cassini_flyby.csv')  # get file's directory, up one level, /data/star_data.csv
+	flyby_csv_file = os.path.join(os.path.dirname(__file__), 'data', 'cassini_flyby.csv')  # get file's directory, up one level, /data/*.csv
 	flyby_dataframe = pd.read_csv(flyby_csv_file)
 	for index, row in flyby_dataframe.iterrows():
 		row = row.tolist()
@@ -29,10 +29,55 @@ if __name__ == '__main__':
 	flyby_observiation_num = "0065"
 
 	if flyby_observiation_num not in avaliable_observation_numbers:
-		print("Observation number '{0}' NOT FOUND in avaiable observation numbers: {1}".format(flyby_observiation_num, avaliable_observation_numbers))
+		print("Observation number '{0}' NOT FOUND in avaiable observation numbers: {1}\n".format(flyby_observiation_num, avaliable_observation_numbers))
 		exit()
 	else:
-		print("Observation number '{0}' FOUND in avaiable observation numbers: {1}".format(flyby_observiation_num, avaliable_observation_numbers))
+		print("Observation number '{0}' FOUND in avaiable observation numbers: {1}\n".format(flyby_observiation_num, avaliable_observation_numbers))
+
+	import csv 
+	from datetime import datetime, timedelta
+	days_between_checking_jpl_website = 2 # set to 0 to re-run currently without waiting
+	x_days_ago = datetime.now() - timedelta(days=days_between_checking_jpl_website)
+	
+	filetime = datetime.fromtimestamp(os.path.getctime(os.path.join(os.path.dirname(__file__), 'data', 'coradr_jpl_options.csv')))
+	if filetime < x_days_ago:
+		# File it more than X days old
+		print("file is older than {0} days, running html capture for CORADR options:".format(days_between_checking_jpl_website))
+	
+		# BeautifulSoup web scrapping to find observation file number full title
+		print("Retrieving observation information from pds-imaging.jpl.nasa.gov/ata/cassini/cassini_orbital....")
+		cassini_root_url = "https://pds-imaging.jpl.nasa.gov/data/cassini/cassini_orbiter"
+		from bs4 import BeautifulSoup
+		cassini_html = request.urlopen(cassini_root_url).read()
+		soup = BeautifulSoup(cassini_html, 'html.parser')
+		table = soup.find('table', {"id": "indexlist"})
+		table_text = (table.text).split("\n")
+		coradr_options = []
+		for txt in table_text:
+			if 'CORADR' in txt:
+				coradr_title = txt.split('/')[0]
+				if '.' not in coradr_title:
+					coradr_options.append(coradr_title)
+		print(coradr_options)
+		# Wrte to CSV
+		header_options = ["CORADR ID Options"]
+		df = pd.DataFrame(coradr_options, columns=header_options)
+		df = df.sort_values(by=["CORADR ID Options"])
+		df.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'coradr_jpl_options.csv'), header=header_options, index=False)
+
+	# Read from CSV
+	jpl_coradr_options = []
+	coradr_csv_file = os.path.join(os.path.dirname(__file__), 'data', 'coradr_jpl_options.csv')  # get file's directory, up one level, /data/*.csv
+	coradr_dataframe = pd.read_csv(coradr_csv_file)
+	for index, row in coradr_dataframe.iterrows():
+		row = row.tolist()
+		jpl_coradr_options.append(row[0])
+
+	find_cordar_listing = 'CORADR_{0}'.format(flyby_observiation_num)
+	result = list(filter(lambda x: find_cordar_listing in x, jpl_coradr_options))
+	print(result)
+	exit()
+
 
 	label_url = "https://pds-imaging.jpl.nasa.gov/data/cassini/cassini_orbiter/CORADR_{0}_V03/DATA/BIDR/BIBQD05S184_D065_T008S03_V03.LBL".format(flyby_observiation_num)
 	label_name = label_url.split("/")[-1].split(".")[0] + ".txt"
