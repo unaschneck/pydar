05310fab53324ec8957cd2df50f17f85e0a7a130
unaschneck <24469269+unaschneck@users.noreply.github.com>
2023-01-03 19:52:34 -0500

    turned meta retrieval into function call

--- pydar/get_metadata.py
+++ pydar/get_metadata.py
@@ -1,6 +1,12 @@
+# Extract metadata from BIDR files
+
 from planetaryimage import PDS3Image
 
-testfile = 'BIBQD49N071_D035_T00AS01_V03.img'
-image = PDS3Image.open(testfile)
+def getFlybymeta()
+    testfile = 'BIBQD49N071_D035_T00AS01_V03.img'
+    image = PDS3Image.open(testfile)
 
-print(vars(image))
+
+    all_var = vars(image)
+    
+    return all_var
